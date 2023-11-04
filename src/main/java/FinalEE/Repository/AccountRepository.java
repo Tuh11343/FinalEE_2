2d59790276f4629db99be80bb247f8aad97290da
Tuh <dotuan2k2@gmail.com>
2023-11-04 23:12:43 +0700

    Cap nhat xu ly login

--- src/main/java/FinalEE/Repository/AccountRepository.java
+++ src/main/java/FinalEE/Repository/AccountRepository.java
@@ -3,9 +3,13 @@ package FinalEE.Repository;
 
 import FinalEE.Entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer>{
+
+    /*@Query("SELECT ac from Account ac where ac.name=?1 and ac.password=?2")*/
+    public Account findByNameAndPassword(String name,String password);
 }
